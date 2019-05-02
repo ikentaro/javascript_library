0a96540b385bc9cd70e2020a4bd28f42743509da
Inoue <forl.head.officer@gmail.com>
2019-05-02 16:36:13 +0900

    solver/bisection.js 実装完了 コメントアウト

--- src/math/solver/bisection.js
+++ src/math/solver/bisection.js
@@ -1,15 +1,13 @@
 const MAX_COUNT=10000;
 
 const bisection=(func, x0, x1, thre=1.0e-8)=>{
-    console.log('===== solver.bysection START  =====');
+//    console.log('===== solver.bysection START  =====');
     let y0=func(x0), y1=func(x1);
     let xmid=0.5*(x1+x0);
     let ymid=func(xmid);
-    if( y0*y1>0 ){
-	throw new Error(`solver.bysection f(x0=${x0})=${y0}  f(x1=${x1})=${y1} was invalid parameter`);
-    }
+    if( y0*y1>0 ){　throw new Error(`solver.bysection f(x0=${x0})=${y0}  f(x1=${x1})=${y1} was invalid parameter`); }
 
-    console.log(`x0=${x0} xmid=${xmid} x1=${x1} y0=${y0} ymid=${ymid} y1=${y1}`);
+//    console.log(`x0=${x0} xmid=${xmid} x1=${x1} y0=${y0} ymid=${ymid} y1=${y1}`);
     const isBreak=(ans0, ans_mid, ans1)=>{
 	if( Math.abs(ans0, ans_mid)>thre ) return false;
 	if( Math.abs(ans1, ans_mid)>thre ) return false;
@@ -18,7 +16,7 @@ const bisection=(func, x0, x1, thre=1.0e-8)=>{
 
     for( let count=0; ; count++ ){
 	if( isBreak(y0, ymid, y1 ) ) break;
-	console.log(`i=${count}  x0=${x0} xmid=${xmid} x1=${x1} y0=${y0} ymid=${ymid} y1=${y1}`);
+//	console.log(`i=${count}  x0=${x0} xmid=${xmid} x1=${x1} y0=${y0} ymid=${ymid} y1=${y1}`);
 	if( y0*ymid>0 ){
 	    y0=ymid;
 	    x0=xmid;
@@ -32,8 +30,8 @@ const bisection=(func, x0, x1, thre=1.0e-8)=>{
 	
 	if( count===MAX_COUNT ) throw new Error(`solver.bisection iteration=${count} over`);
     }
-    console.log(`f(x0=${x0})=${y0}`, `f(xmid=${xmid})=${ymid}`, `f(x1=${x1})=${y1}`);
-    console.log('===== solver.bysection FINISH =====');
+//    console.log(`f(x0=${x0})=${y0}`, `f(xmid=${xmid})=${ymid}`, `f(x1=${x1})=${y1}`);
+//    console.log('===== solver.bysection FINISH =====');
     return xmid;
 }
 
