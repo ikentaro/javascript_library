cd0f50e9147be2f835bfcff6a20c57fc3468eddc
takeyasu <forl.head.officer@gmail.com>
2020-01-09 16:14:32 +0900

    three_earth.html スケールファクターUI 追加

--- copyToHeroku.sh
+++ copyToHeroku.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-cp dist/* /home/kentaro/local/heroku/takeyasu-app/public/dist/
+cp -a dist/* /home/kentaro/local/heroku/takeyasu-app/public/dist/
+cp -a earth.png /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a three_earth.html /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a three_earth.css                                          /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a node_modules/three/build/three.min.js                    /home/kentaro/local/heroku/takeyasu-app/public/node_modules/three/build/
+cp -a node_modules/three/examples/js/controls/OrbitControls.js /home/kentaro/local/heroku/takeyasu-app/public/node_modules/three/examples/js/controls/
--- js/three_earth.js
+++ js/three_earth.js
@@ -10,22 +10,23 @@ window.addEventListener('load', async ()=>{
     three.camera.perspective({ pos: [ 60, 50, 60 ], lookAt: [ 0, 0, 0 ] });
 
     three.axis(50);
-    const simTimeElem=document.getElementById('sim-time');
-    const fpsElem=document.getElementById('fps');
-    
+    const simTimeElem=document.getElementById('sim-time'), fpsElem=document.getElementById('fps'), rangeElem=document.getElementById('range-scale'), scaleElem=document.getElementById('scale');
     const R=50, r=10;
-    three.light.point(0xFFFFFF, 1.0).position.set( 11, 0, 0);
-    three.light.point(0xFFFFFF, 1.0).position.set(-11, 0, 0);
-    three.light.point(0xFFFFFF, 1.0).position.set(0,  11, 0);
-    three.light.point(0xFFFFFF, 1.0).position.set(0, -11, 0);
-    three.light.point(0xFFFFFF, 1.0).position.set(0, 0,  11);
-    three.light.point(0xFFFFFF, 1.0).position.set(0, 0, -11);
-    
+
+    setLight();
     const earthTexture= await three.texture('earth.png');
     const earth=three.mesh(three.geom.sphere(3, 10, 10), three.material.standard({ map: earthTexture }));
     const moon=three.mesh(three.geom.sphere(1, 10, 10), three.material.standard({ color: 'yellow',  }));
     const sun=three.mesh(three.geom.sphere(10, 10, 10), three.material.standard({ color: 'red', opacity: 0.5, transparent: true, side: THREE.DoubleSide }), { pos: [0, 0, 0] });
     anime.request(calc);
+
+    changeScale();
+    rangeElem.addEventListener('change', changeScale);
+    
+    function changeScale(){
+	scaleFactor=0.1*Number(scaleElem.value);
+	scaleElem.value=rangeElem.value;
+    }
     
     function setCam(thetaEarth){
 	const selected=[ ...document.getElementsByName('camera') ].find(a=> a.checked);
@@ -46,7 +47,7 @@ window.addEventListener('load', async ()=>{
 	    three.config.camera.lookAt(R*Math.cos(thetaEarth), 0. ,R*Math.sin(thetaEarth));
 	}
 //	else alert('カメラエラー');
-	console.log(selected.value);	
+//	console.log(selected.value);	
     }
     function calc(){
 	const now=performance.now();
@@ -54,32 +55,30 @@ window.addEventListener('load', async ()=>{
 	while( times[0]<now-1000 ) times.shift();
 	
 	let innerTime=scaleFactor*performance.now();
-	const time=innerTime%24;
-	const thetaEarth=2.0*Math.PI*innerTime/(365.0*24.0);
-	const localTheta=2.0*Math.PI*time/24;
-	const thetaMoon =2.0*Math.PI*innerTime/(365*24/12);
+	const time=innerTime%24, thetaEarth=2.0*Math.PI*innerTime/(365.0*24.0), localTheta=2.0*Math.PI*innerTime/24, thetaMoon =2.0*Math.PI*innerTime/(365*24/12);
 	innerTime=Math.ceil(innerTime/24);
 	let day=innerTime%365, month;
-
 	const numOfDays=[ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 	for( let i=0; numOfDays.length; i++ ){
 	    if( numOfDays[i]>day ){ month=i+1; break; }
 	    else day-=numOfDays[i];
 	}
-	
-	const moonX=R*Math.cos(thetaEarth)+r*Math.cos(thetaMoon);
-	const moonZ=R*Math.sin(thetaEarth)+r*Math.sin(thetaMoon);
-	
 	earth.position.set(R*Math.cos(thetaEarth), 0.0, R*Math.sin(thetaEarth));
-
-	moon.position.set(moonX, 0.0, moonZ);
+	moon.position.set(R*Math.cos(thetaEarth)+r*Math.cos(thetaMoon), 0, R*Math.sin(thetaEarth)+r*Math.sin(thetaMoon));
+	
 	const year=2019+Math.ceil(innerTime/365);
-	simTimeElem.value=`${year}/${month}/${day} ${time}`;
+	simTimeElem.value=`${year}/${month}/${day} ${Math.ceil(time)}`;
 	fpsElem.value=times.length;
 	earth.rotateY(localTheta);
 	three.render();
 
 	setCam(thetaEarth);
+    }
+
+    function setLight(){
+	const r=11;
+	for( let theta=0.0; theta<2.*Math.PI; theta+=0.8 ) for( let phi=0.0; phi<Math.PI; phi+=0.8 )
+	    three.light.point(0xFFFFFF, 1.0, 65).position.set(r*Math.sin(theta)*Math.sin(phi), r*Math.sin(theta)*Math.cos(phi), r*Math.cos(phi));
     }
     console.log('===== Three.js Wrapper earth FINISH =====');
 });
