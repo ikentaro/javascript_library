135838457b1ba183257dd92eb4826e9ba4a3bf2e
takeyasu <forl.head.officer@gmail.com>
2020-01-10 09:30:08 +0900

    mic.html snap.svg読み込み先変更

--- copyToHeroku.sh
+++ copyToHeroku.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-
-cp -a dist/* /home/kentaro/local/heroku/takeyasu-app/public/dist/
-cp -a earth.png /home/kentaro/local/heroku/takeyasu-app/public/
-cp -a three_earth.html /home/kentaro/local/heroku/takeyasu-app/public/
-cp -a three_earth.css                                          /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a dist/*                                                   /home/kentaro/local/heroku/takeyasu-app/public/dist/
+cp -a earth.png                                                /home/kentaro/local/heroku/takeyasu-app/public/
 cp -a node_modules/three/build/three.min.js                    /home/kentaro/local/heroku/takeyasu-app/public/node_modules/three/build/
+cp -a node_modules/snapsvg/dist/snap.svg-min.js                /home/kentaro/local/heroku/takeyasu-app/public/node_modules/snapsvg/dist/
 cp -a node_modules/three/examples/js/controls/OrbitControls.js /home/kentaro/local/heroku/takeyasu-app/public/node_modules/three/examples/js/controls/
+
+cp -a three_earth.html                                         /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a three_earth.css                                          /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a js/three_earth.js                                        /home/kentaro/local/heroku/takeyasu-app/public/js/
+
+cp -a three_lorenz.html                                        /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a three_lorenz.css                                         /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a js/three_lorenz.js                                       /home/kentaro/local/heroku/takeyasu-app/public/js/
+
+
+cp -a mic.html                                        /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a mic.css                                         /home/kentaro/local/heroku/takeyasu-app/public/
+cp -a js/mic.js                                       /home/kentaro/local/heroku/takeyasu-app/public/js/
+
+
