198b742f7b5c7c3653df931e55a3362f25396bb4
takeyasu <forl.head.officer@gmail.com>
2020-01-11 15:54:42 +0900

    mic.html に録音機能を追加

--- js/mic.js
+++ js/mic.js
@@ -6,11 +6,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const graWave=svg.graph(0, 0, 0.5*anaCanvas.clientWidth, anaCanvas.clientHeight, { ymin: -1, ymax: 1, xmin: 0, xmax: analyzer.fftSize/audio.context.sampleRate });
     const graFreq=svg.graph(0.5*anaCanvas.clientWidth, 0, 0.5*anaCanvas.clientWidth, anaCanvas.clientHeight, { ymin: -200, ymax: -30, xmin:0, xmax: audio.context.sampleRate/2.0 });
     audio.viewer.analyzer(analyzer, graWave, graFreq);
-    
+
+    const chunks=[];
     navigator.mediaDevices.getUserMedia({ audio: true }).then(stream=>{
 	const mediaSS=audio.create.mediaStreamSource(stream);
-	
+	const recorder=new MediaRecorder(stream);
+	if( confirm('録音しますか❓') ) recorder.start();
 	mediaSS.connect(analyzer).connect(audio.context.destination);
-    });    
+//	recorder.addEventListener('dataavailable', (e)=>{ console.log(e.data); chunks.push(e.data); });
+	recorder.ondataavailable = (e)=>{ console.log(e.data); chunks.push(e.data); };
+    });
     console.log('===== mic FINISH =====');
 });
