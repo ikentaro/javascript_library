12e31884371dfca97833000c524b073d106e9c36
takeyasu <forl.head.officer@gmail.com>
2020-01-22 21:19:15 +0900

    three_body2.html 実装 three_body.htmlとズレあり

--- js/three_body2.js
+++ js/three_body2.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', ()=>{
-    console.log('===== 3-body simulation START  =====');
+    console.log('===== 3-body simulation Runge-Kutta START  =====');
     let scalingF=0.001;
     let time=performance.now(), G=3.0e3;
     
-    const pos=[ [ 0, 0, 0 ], [0, 0, 0], [0, 0, 0] ], vel=[ [ 0, 0, 0 ], [0, 0, 0], [0, 0, 0] ], mass=[ 0, 0, 0 ];
+    const pos=[ math.vector(0, 0, 0), math.vector(0, 0, 0), math.vector(0, 0, 0) ], vel=[ math.vector(0, 0, 0), math.vector(0, 0, 0), math.vector(0, 0, 0) ], mass=[ 0, 0, 0 ];
     const posElem=[], velElem=[], massElem=[];
     const gElem=document.getElementById('gravity-const');
     for( let i=0; i<3; i++ ){
@@ -27,34 +27,35 @@ window.addEventListener('DOMContentLoaded', ()=>{
     
     document.getElementById('start-btn').addEventListener('click', ()=>{
 	time=performance.now();
-	anime.request(calc);
+	const func=()=>{
+	    const now=performance.now();
+	    const dt=scalingF*(time-now);
+	    time=now;
+	    [ pos[0], pos[1], pos[2], vel[0], vel[1], vel[2] ] = math.rungeKutta.iterate(calc, [ pos[0], pos[1], pos[2], vel[0], vel[1], vel[2] ], 1, dt);
+	    setPos();
+	    three.render();
+	    return isContinue();
+	}
+	anime.request(func);
     });
 
-    function calc(){
-	const now=performance.now();
-	const dt=scalingF*(time-now);
-	time=now;
-//	console.log('===========\ndt   :', dt, 'now :', now, 'time', time);	
-	for( let i=0; i<3; i++ ){
-	    let force=[ 0, 0, 0 ];
-	    for( j=0; j<3; j++ ){
-		pos[i][j]+=dt*vel[i][j];
+    function calc(pos0, pos1, pos2, vel0, vel1, vel2){
+	const result=[ vel0, vel1, vel2 ];
+	for( let i=0; i<vel.length; i++ ){
+	    let force=math.vector(0, 0, 0);
+	    for( let j=0; j<vel.length; j++ ){
 		if( i===j ) continue;
-		const r=Math.sqrt(Math.pow(pos[i][0]-pos[j][0], 2)+Math.pow(pos[i][1]-pos[j][1], 2)+Math.pow(pos[i][2]-pos[j][2], 2));
-		force[0]+=G*mass[i]*mass[j]*(pos[j][0]-pos[i][0])/Math.pow(r, 3);
-		force[1]+=G*mass[i]*mass[j]*(pos[j][1]-pos[i][1])/Math.pow(r, 3);
-		force[2]+=G*mass[i]*mass[j]*(pos[j][2]-pos[i][2])/Math.pow(r, 3);
+		force=math.add(force, math.mul(math.sub(pos[j], pos[i]), mass[i], mass[j], math.div(G, math.pow(math.sub(pos[j], pos[i]).abs(), 3))));
 	    }
-	    vel[i][0]+=dt*force[0]/mass[i];
-	    vel[i][1]+=dt*force[1]/mass[i];
-	    vel[i][2]+=dt*force[2]/mass[i];
+	    console.log(force);
+	    result.push(math.div(force, mass[i]));
 	}
-	setPos();
-
-	return isContinue(); 
+//	console.log('calc result', result);
+	return result;
     }
-    console.log('===== 3-body simulation FINISH =====');
-    function isContinue(){
+	
+    console.log('===== 3-body simulation Runge-Kutta FINISH =====');
+    function isContinue(){	
 	if( Math.sqrt(pos[0][0]*pos[0][0]+pos[0][1]*pos[0][1]+pos[0][2]*pos[0][2])>100 &&
 	    Math.sqrt(pos[1][0]*pos[1][0]+pos[1][1]*pos[1][1]+pos[1][2]*pos[1][2])>100 &&
 	    Math.sqrt(pos[2][0]*pos[2][0]+pos[2][1]*pos[2][1]+pos[2][2]*pos[2][2])>100 ) return false;
@@ -67,18 +68,20 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     function getParam(){
 	G=1.0e3*Number(gElem.value)
-	console.log('gravity-param', G);
 	for( let i=0; i<3; i++ ){
 	    mass[i]=Number(massElem[i].value);
 	    for( let j=0; j<3; j++ ){
 		pos[i][j]=Number(posElem[i][j].value);
 		vel[i][j]=Number(velElem[i][j].value);
-		console.log('x['+i+']['+j+']', pos[i][j]);
-		console.log('v['+i+']['+j+']', vel[i][j]);
 	    }
-	    console.log('m['+i+']', mass[i]);
 	}
-
+    }
+    function printParam(){
+	console.log('gravity-param', G);
+	console.log('===== print position =====');
+	for( let i=0; i<3; i++ ) for( let j=0; j<3; j++ ) console.log(' x['+i+']['+j+']', pos[i][j]);
+	console.log('===== print velocity =====');
+	for( let i=0; i<3; i++ ) for( let j=0; j<3; j++ ) console.log(' v['+i+']['+j+']', vel[i][j]);
     }
 });
 
--- src/math/div.js
+++ src/math/div.js
@@ -1,10 +1,16 @@
+import Vector from './Vector.js'
+
 const div=(...args)=>{
     const div2=(a,b)=>{
 	if( typeof a==='number' && typeof b==='number' ) return a/b;
 	else if( typeof a.div==='function' ) return a.div(b);
 	else if( typeof b.mul==='function' && typeof b.reverse==='function' ) return b.reverse().mul(a);
-
-	throw new Error('! math.dev invalid parameter');
+	else if( Array.isArray(a) && a.every(b=> typeof b==='number') && typeof b=='number') return new Vector(...a.map(a=> a/b));
+	console.log('a=', a);
+	console.log('a.every', a.every(b=> typeof b==='number'));
+	console.log('b=', b);
+	console.log('b=', typeof(b));
+	throw new Error('! math.div invalid parameter');
     }
 
     return args.reduce((acc, a)=> div2(acc, a));
