60d4b3ff96820ff6950bf18e2c6a60947681027b
takeyasu <forl.head.officer@gmail.com>
2020-01-29 15:24:59 +0900

    debug comment (comment out)

--- src/math/rungeKutta/step.js
+++ src/math/rungeKutta/step.js
@@ -6,10 +6,28 @@ import mul from '../mul.js'
 const step=(func, args, delta=1.0e-8)=>{
     if( typeof func==='function' && Array.isArray(args) ){
 	const k1=func(...args);
+	// console.log('==== k1 ===========');
+	// k1.forEach((a, i)=>{ console.log(i, a) });
+	// console.log('===================');
+	
 	const k2=func(...args.map((a, i)=> add(a, mul(0.5, delta, k1[i]))));
+	// console.log('==== k2 ===========');
+	// k2.forEach((a, i)=>{ console.log(i, a) });	
+	// console.log('===================');
+	
 	const k3=func(...args.map((a, i)=> add(a, mul(0.5, delta, k2[i]))));
+	// console.log('==== k3 ===========');
+	// k3.forEach((a, i)=>{ console.log(i, a) });
+	// console.log('===================');
+	
 	const k4=func(...args.map((a, i)=> add(a, mul(delta, k3[i])))); 
+	// console.log('==== k4 ==========');
+	// k4.forEach((a, i)=>{ console.log(i, a) });
+	// console.log('===================');
 
+	// console.log('==== Runge-Kutta step result =====');
+	// args.map((a, i)=> add(a, div(mul(delta, add(k1[i], mul(2, k2[i]), mul(2, k3[i]), k4[i])), 6))).forEach((a, i)=>{ console.log(i, a) });
+	// console.log('==================================');
 	return args.map((a, i)=> add(a, div(mul(delta, add(k1[i], mul(2, k2[i]), mul(2, k3[i]), k4[i])), 6)));
     }
 
